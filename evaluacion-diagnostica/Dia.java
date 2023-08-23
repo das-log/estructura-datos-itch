9ebbbd33a61ce63d843efa3e259fdb3b7ed822ea
darkangelsinner <100170221+das-log@users.noreply.github.com>
2023-08-22 23:33:19 -0500

    Create Dia.java

---
+++ evaluacion-diagnostica/Dia.java
@@ -0,0 +1,74 @@
+/*
+ * Se piden ingresar 2 datos por dia: tiempo, velocidad media.
+ * las calorias consumidas se calcularan por dia mediante una funcion.
+ * como tenemos una distancia especificada de 10,000 metros y un rango de velocidad de 4 - 6 km/h, 
+ * podemos calcular el tiempo usando valores de ese rango.
+ * 
+ * se podria implementar una sobrecarga de constructor para calcular la velocidad usando el tiempo si se tuviera.
+ * 
+ */
+import java.util.Scanner;
+ /*se crea la clase dia */
+
+public class Dia{
+   private float tiempo;
+   private int velocidad;
+   private int peso;
+   private float calPerdidas;
+
+   public Dia(int velocidad, int peso){
+      this.velocidad = velocidad;
+      this.peso = peso;
+      this.tiempo = calculoTiempo();
+      this.calPerdidas = caloriasPerdidas();
+   }
+
+
+   public float calculoTiempo(){ //funcion para calcular tiempo
+
+      /* consideramos 10km porque es equivalente a 10,000 metros, debido a la unidad de velocidad */
+      float tiempo = 10f/velocidad; 
+      return tiempo;
+    }
+
+    
+    public float caloriasPerdidas(){ //funcion para calcular calorias perdidas
+      float calPerdidas = (float) (1.03 * peso * 10);
+      return calPerdidas;
+    }
+
+    //metodos accesores
+
+    public float getTiempo(){
+      return tiempo;
+    }
+
+    public float getCalPerdidas(){
+      return calPerdidas;
+    }
+
+public static void main(String[] args) {
+ //creamos arreglo con cada dia de entrenamiento
+ Scanner entrada = new Scanner(System.in);
+ System.out.println("Ingrese cuantos dias entreno: ");
+ int dias = entrada.nextInt();
+
+ 
+ Dia[] entrenamiento = new Dia[dias]; //creamos un arreglo de tipo dia
+
+ //llenamos el arreglo 
+ for (int i = 0; i < dias; i++) {
+  System.out.println("Ingrese la velocidad para el día " + (i + 1) + ": ");
+  int velocidad = entrada.nextInt();
+  
+  System.out.println("Ingrese el peso para el día " + (i + 1) + ": ");
+  int peso = entrada.nextInt();
+
+  // Creamos un objeto Dia con los valores ingresados
+  entrenamiento[i] = new Dia(velocidad, peso);
+
+ }
+
+}
+
+}
