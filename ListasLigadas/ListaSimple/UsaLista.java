020fb98d25a51d83e1c14b50fb75efb48ecc7e60
Jonathan Gutierrez <100170221+das-log@users.noreply.github.com>
2023-11-30 22:26:42 -0500

    Add files via upload

---
+++ ListasLigadas/ListaSimple/UsaLista.java
@@ -0,0 +1,17 @@
+public class UsaLista{
+
+	public static void main(String[] args){
+		ListaSimple lista = new ListaSimple();
+
+		lista.insertaPrimerNodo("H");
+		lista.insertaAntesPrimerNodo("O");
+		lista.insertaAlFinal("Y");
+		lista.insertaEntreNodos("R", "A");
+lista.obtenerNodoPorPosicion(1);
+lista.obtenerPosicionesNodos();
+		lista.imprimir();
+		
+	}
+
+
+}
