2f3e2c818fda484c01e094a840a02bf1bea02bce
Jonathan Gutierrez <100170221+das-log@users.noreply.github.com>
2023-12-01 19:23:00 -0500

    Update ListaSimple.java

--- ListasLigadas/ListaSimple/ListaSimple.java
+++ ListasLigadas/ListaSimple/ListaSimple.java
@@ -1,34 +1,33 @@
-public class ListaSimple{
+public class ListaSimple {
 	Node top;
 
-	//Métodos para los casos de inserción de nodos
-	public boolean insertaPrimerNodo(String dato){
-		if (top == null) { //verificar si la lista esta vacia
+	// Métodos para los casos de inserción de nodos
+	public boolean insertaPrimerNodo(String dato) {
+		if (top == null) { // verificar si la lista esta vacia
 			top = new Node();
 			top.name = dato;
 			top.next = null;
 
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
 
-	public void insertaAntesPrimerNodo(String nombre){
-		Node temp; 
-		temp = new Node ();
+	public void insertaAntesPrimerNodo(String nombre) {
+		Node temp;
+		temp = new Node();
 		temp.name = nombre;
 		temp.next = this.top;
 		this.top = temp;
 		temp = null;
 	}
 
-	public void insertaAlFinal(String nombre){
-		Node temp = new Node ();
+	public void insertaAlFinal(String nombre) {
+		Node temp = new Node();
 		temp.name = nombre;
 		temp.next = null;
-		//Node temp2;
+		// Node temp2;
 		Node temp2 = this.top;
 
 		while (temp2.next != null)
@@ -39,94 +38,93 @@ public class ListaSimple{
 		temp2 = null;
 	}
 
-	public boolean insertaEntreNodos(String nombre, String buscado){
+	public boolean insertaEntreNodos(String nombre, String buscado) {
 		Node temp = new Node();
 		temp.name = nombre;
 		Node temp2 = this.top;
 
-		//boolean NodoNoEncontrado = true;
+		// boolean NodoNoEncontrado = true;
 
-		while ( (temp2 != null) 
-				&& temp2.name.equals(buscado) == false ) {	
-		         temp2 = temp2.next;
+		while ((temp2 != null)
+				&& temp2.name.equals(buscado) == false) {
+			temp2 = temp2.next;
 		}
 
-		if (temp2 != null){  //Nodo buscado se encontró
+		if (temp2 != null) { // Nodo buscado se encontró
 			temp.next = temp2.next;
 			temp2.next = temp;
 			temp = null;
 			temp2 = null;
 			return true;
-		}
-		else return false;
-	} 
+		} else
+			return false;
+	}
 
-	public void imprimir(){
-		for (Node temp = this.top; temp != null; temp = temp.next){
+	public void imprimir() {
+		for (Node temp = this.top; temp != null; temp = temp.next) {
 			System.out.print("[ " + temp.name + " ] -> ");
 		}
 
-		System.out.print("[X]\n"); 
+		System.out.print("[X]\n");
 	}
 
-	public String toString(){
+	public String toString() {
 		String cadAux = "";
-		for (Node temp = this.top; temp != null; temp = temp.next){
+		for (Node temp = this.top; temp != null; temp = temp.next) {
 			cadAux += "[ " + temp.name + " ] -> ";
 		}
 
-		cadAux += "[X]\n"; 
+		cadAux += "[X]\n";
 
 		return cadAux;
 	}
 
-	
-	//Métodos de borrado
-	public void borrarPrimerNodo(){
+	// Métodos de borrado
+	public void borrarPrimerNodo() {
 		this.top = this.top.next;
 	}
 
-	//Borrar cualquier nodo que no sea el primero
-	public boolean borrarCualquierNodo(String buscado){
+	// Borrar cualquier nodo que no sea el primero
+	public boolean borrarCualquierNodo(String buscado) {
 		Node temp = this.top;
 
-		while ( (temp != null) 
-				&& temp.name.equals(buscado) == false ) {	
-		         temp = temp.next;
+		while ((temp != null)
+				&& temp.name.equals(buscado) == false) {
+			temp = temp.next;
 		}
 
-		if (temp != null){  //Nodo buscado se encontró
+		if (temp != null) { // Nodo buscado se encontró
 			temp.next = temp.next.next;
 			temp = null;
-			
+
 			return true;
-		}
-		else return false;
+		} else
+			return false;
 	}
 
-	//se implementa como dos funciones diferentes
+	// se implementa como dos funciones diferentes
 
-	public void obtenerPosicionesNodos(){
+	public void obtenerPosicionesNodos() {
 		Node temp = this.top;
 		int c = 0;
-		while (temp != null){
-			System.out.println("[posicion: "+c+" , nodo: " +temp.name+" ]");
+		while (temp != null) {
+			System.out.println("[posicion: " + c + " , nodo: " + temp.name + " ]");
 			temp = temp.next;
 			c++;
 		}
-		
+
 	}
 
-	//buscar nodo por posicion
-	public Node obtenerNodoPorPosicion(int posicion){
+	// buscar nodo por posicion
+	public Node obtenerNodoPorPosicion(int posicion) {
 		Node temp = new Node();
-	temp = this.top;
+		temp = this.top;
 		int c = 0;
 		while (temp != null) {
-			if (c == posicion){
+			if (c == posicion) {
 				System.out.println(temp.name);
 				return temp;
-				
+
 			}
 			temp = temp.next;
 			c++;
@@ -136,49 +134,69 @@ public class ListaSimple{
 		return null;
 	}
 
-	//insertar nuevo nodo antes del ultimo
-	public void insertarAntesUltimo(String nombre){
-	Node temp = new Node();
-	temp.name = nombre;
-	temp.next = null;
+	// insertar nuevo nodo antes del ultimo
+	public void insertarAntesUltimo(String nombre) {
+		Node temp = new Node();
+		temp.name = nombre;
+		temp.next = null;
 
-	//comprobamos si la lista esta vacia
-	if (top == null){
-		top = temp;
-		return;
+		// comprobamos si la lista esta vacia
+		if (top == null) {
+			top = temp;
+			return;
+		}
+
+		// si solo hay un nodo, se inserta antes de el
+		if (top.next == null) {
+			temp.next = top;
+			top = temp;
+			return;
+		}
+
+		Node temp2 = this.top;
+		Node anterior = null;
+
+		while (temp2.next != null) {
+			anterior = temp2;
+			temp2 = temp2.next;
+		}
+
+		temp.next = temp2;
+		anterior.next = temp;
 	}
+
+	// intercambiar nodo existente por uno nuevo
+	public void intercambiarNodos(String existente, String nombre) {
+		Node temp = new Node();
+		temp.name = nombre;
+		temp.next = null;
+
+		// debemos buscar el nodo anterior al existente tambien
+		Node nodoAnterior = null;
+		Node nodoExistente = this.top;
+
+		while (nodoExistente != null && !nodoExistente.name.equals(existente)) {
+			nodoAnterior = nodoExistente;
+			nodoExistente = nodoExistente.next;
+
+		}
+
+		if (nodoExistente == null) {
+			System.out.println("El nodo existente no fue encontrado.");
+			return;
+		}
+
+		temp.next = nodoExistente.next;
+
+		// si el nodo existente no es el inicial
+		if (nodoAnterior != null) {
+			nodoAnterior.next = temp;
+		} else {
+			this.top = temp;
+
+		}
+
 	
-	
-	//si solo hay un nodo, se inserta antes de el
-	if (top.next == null){
-		temp.next = top;
-		top = temp;
-		return;
-	}
-
-	Node temp2 = this.top;
-	Node anterior = null;
-
-	while(temp2.next != null){
-		anterior = temp2;
-		temp2 = temp2.next;
-	}
-
-	temp.next = temp2;
-	anterior.next = temp;
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
